7a75e35f2c43e075f375b819fbfcbae998734b73
Мехрафруз <mehrafruz@MacBook-Air-Mehrafruz.local>
2022-04-05 21:18:18 +0300

    md-11: Практика по гиту

--- SharePay/SceneDelegate.swift
+++ SharePay/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 window = UIWindow(windowScene: windowScene)
                 window?.rootViewController = ViewController()
                 window?.makeKeyAndVisible()
+        ///Some comment for git practic 
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
